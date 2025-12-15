ad41221d636d82be51f94a23271fe926714171ab
Mehedi Hasan Shuvo <102133774+shawonMadaripur@users.noreply.github.com>
2025-12-15 23:04:30 +0600

    add delete button and wish button

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ const countryDisplayOnTable = (data) => {
       <td>${element.Institute}</td>
       <td>${element.Professors}</td>
       <td>${element.Research_Area}</td>
-      <td>${element.URL} <button type="button" class="btn btn-primary" onclick="clickWish(${element.id})">add to wish</button> </td>
+      <td><a target="_blank" href="${element.URL}">${element.URL}</a> <button type="button" class="btn btn-primary" onclick="clickWish(${element.id})">add to wish</button> </td>
     `
 
     parent.appendChild(tr)
@@ -82,6 +82,82 @@ const countryDisplayOnTable = (data) => {
 
 const clickWish = (id) => {
   wishListSet.add(id);
+  
+  // put wishlistset into locatstorage
+  const wishArray = Array.from(wishListSet);
+  localStorage.setItem("wishList", JSON.stringify(wishArray));
+  document.getElementById("count_wish_number").innerText = "";
+  document.getElementById("count_wish_number").innerText = wishArray.length;
+  // console.log("Saved to localStorage:", wishArray);
+}
+
+// get wishArray data from local storage
+const getWishList = () => {
+  const data = localStorage.getItem("wishList");
+
+  if (!data) return [];
+
+  return JSON.parse(data);
+};
+
+// show all the data into wish.html table
+const displayWishTable = () => {
+  const wishIds = getWishList();
+
+  if (wishIds.length === 0) {
+    console.log("Wishlist is empty");
+    return;
+  }
+
+  // Fetch all data
+  fetch("https://versity-search.onrender.com/versitylist/list/")
+    .then(res => res.json())
+    .then(data => {
+      const wishedItems = data.filter(item =>
+        wishIds.includes(item.id)
+      );
+
+      const tbody = document.querySelector("tbody");
+      tbody.innerHTML = "";
+
+      wishedItems.forEach(item => {
+        const tr = document.createElement("tr");
+        tr.innerHTML = `
+          <td>${item.Country}</td>
+          <td>${item.Institute}</td>
+          <td>${item.Professors}</td>
+          <td>${item.Research_Area}</td>
+          <td>
+          <a href="${item.URL}" target="_blank">${item.URL}</a>
+          <button type="button" class="btn btn-danger" onclick="deleteItem(${item.id})">Delete</button>
+          </td>
+        `;
+        tbody.appendChild(tr);
+      });
+    });
+};
+
+
+// delete item
+const deleteItem = (id) => {
+  // console.log(id)
+  const data = localStorage.getItem("wishList");
+  if(!data)
+    return;
+  let wishList = JSON.parse(data);
+
+  // delete specefic item
+  wishList = wishList.filter(item => item != id);
+  
+  // set list into local store
+  localStorage.setItem("wishList", JSON.stringify(wishList));
+  displayWishTable();
+  
+};
+
+// if wish.html open
+if (window.location.pathname.includes("wish.html")) {
+  displayWishTable();
 }
 
 
